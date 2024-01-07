9e2d37bdb268ca06dd6e0b862ce1dd1e44216b9e
Temirlan <152634778+TimRozz@users.noreply.github.com>
2024-01-07 23:34:34 +0600

    Add files via upload

---
+++ DAY1/src/Task1.java
@@ -0,0 +1,9 @@
+public class Task1 {
+    public static void main(String[] args){
+        int b = 1;
+        while(b<=10){
+            System.out.print("JAVA ");
+            b++;
+        }
+    }
+}
